5f20c96b9b201029175deef9d9e0ca0b9952b6c4
vanessaribeiro03 <ribeirovs2000@outlook.com>
2025-07-10 19:38:29 -0300

    feat: criando model employee

---
+++ Viagium/Models/Employees.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Viagium.Models
+{
+    public class Employees
+    {
+        [Key]
+        public int EmployeeId { get; set; }
+        public int Registration { get; set; }
+        public string? RoleName { get; set; }
+    }
+}
