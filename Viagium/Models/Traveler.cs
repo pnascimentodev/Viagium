a3c60d354ab1079a8ce6f6427ab02933b85205b2
Silas Sefas <silassefas23@gmail.com>
2025-07-14 21:05:13 -0300

    feat: Criando o Model de Traveler

---
+++ Viagium/Models/Traveler.cs
@@ -0,0 +1,16 @@
+﻿namespace Viagium.Models;
+
+public class Traveler
+{
+    [key]
+    public int TravelersId { get; set; }
+    public int reservationId { get; set; } 
+    [Requerid]
+    public string FistName { get; set; }
+    [Requerid]
+    public string LastName { get; set; }
+    [Requerid]
+    public string DocumentNumber { get; set; }
+    [Requerid]
+    public DateTime DateOfBirth { get; set; } //Verificar se vai ser DateTime ou DateOnly
+}
