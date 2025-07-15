4d2625f1d10dd50d6d52bf8d19bcbbb012cbb9c9
Melissa Valentin de Oliveira <melissa.de.oliveira@avanade.com>
2025-07-14 21:33:41 -0300

    feat: Criação Models Review

--- Viagium/Models/Review.cs
+++ Viagium/Models/Review.cs
@@ -1,6 +1,22 @@
-﻿namespace Viagium.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Viagium.Models
 {
     public class Review
     {
+        [Key]
+        public int ReviewId { get; set; }
+
+        [Required]
+        public int ReservationId { get; set; }
+
+        [Required]
+        public int Rating { get; set; }
+
+        [Required]
+        [StringLength(500)]
+        public string Description { get; set; } = string.Empty;
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
     }
 }
