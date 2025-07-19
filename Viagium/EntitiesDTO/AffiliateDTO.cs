1842d02bfd608684365bf9d9deb25b7fbdc42e62
Priscila Nascimento <pnmelo2808@gmail.com>
2025-07-19 15:06:10 -0300

    feat: adicionar modelos Affiliate e AffiliateDTO com propriedades e relacionamento com Hotel

---
+++ Viagium/EntitiesDTO/AffiliateDTO.cs
@@ -0,0 +1,17 @@
+﻿namespace Viagium.EntitiesDTO;
+
+public class AffiliateDTO
+{
+    public string Name { get; set; }
+    public string Cnpj { get; set; }
+    public string CompanyName { get; set; }
+    public string Email { get; set; }
+    public string Phone { get; set; }
+    public string Address { get; set; }
+    public string StateRegistration { get; set; }
+    public bool IsActive { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime? UpdatedAt { get; set; }
+    public DateTime? DeletedAt { get; set; }
+    public List<HotelDTO>? Hotels { get; set; }
+}
