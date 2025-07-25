63afcc8839236c6f56e953bf7d88c90ad545f02d
Priscila Nascimento <pnmelo2808@gmail.com>
2025-07-24 23:18:34 -0300

    chore: add HomeController with root GET endpoint and enable Swagger in all environments

--- Viagium/Program.cs
+++ Viagium/Program.cs
@@ -131,11 +131,10 @@ builder.WebHost.UseUrls($"http://*:{port}");
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
 
 //app.UseHttpsRedirection();
 
