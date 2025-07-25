c037badbc6d0992d68ca11563ff3cc3a54f1149a
Silas Sefas <silassefas23@gmail.com>
2025-07-25 12:08:40 -0300

    refactor: remove ViagiumTests project from solution

---
+++ Viagium/Repository/Interface/IPaymentRepository.cs
@@ -0,0 +1,13 @@
+﻿using Viagium.EntitiesDTO;
+
+namespace Viagium.Repository.Interface;
+
+public interface IPaymentRepository
+{
+    Task <PaymentDTO> CreatePaymentAsync(PaymentDTO payment);
+    Task <PaymentDTO> GetPaymentByIdAsync(int paymentId);
+    Task <PaymentDTO> UpdatePaymentAsync(PaymentDTO payment);
+    Task <PaymentDTO> DeletePaymentAsync(int paymentId);
+    Task <PaymentDTO> GetPaymentByReservationIdAsync(int reservationId);
+    Task <PaymentDTO> RefundPaymentAsync(int paymentId);
+}
