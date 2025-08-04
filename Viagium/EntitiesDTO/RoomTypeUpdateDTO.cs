4ea5b07ba15a1b6e2ce976b8fe2616b63123185b
Silas Sefas <silassefas23@gmail.com>
2025-08-04 15:03:34 -0300

    fix: make ImageUrl property required in RoomTypeUpdateDTO

--- Viagium/EntitiesDTO/RoomTypeUpdateDTO.cs
+++ Viagium/EntitiesDTO/RoomTypeUpdateDTO.cs
@@ -13,6 +13,7 @@ public class RoomTypeUpdateDTO
     public string Name { get; set; } = string.Empty;
     [Required]
     public string Description { get; set; } = string.Empty;
+    [Required]
     public string? ImageUrl { get; set; } = string.Empty;
     public IFormFile? ImageFile { get; set; }
     [Required]
