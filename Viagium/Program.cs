47114f1fd926af9658c0af6e11afe18fa3257a9d
Priscila Nascimento <101822658+pnascimentodev@users.noreply.github.com>
2025-08-07 20:23:42 -0300

    Update Program.cs

--- Viagium/Program.cs
+++ Viagium/Program.cs
@@ -218,12 +218,10 @@ builder.Services.AddHostedService<ReservationStatusBackgroundService>();
 builder.Services.AddHostedService<TravelPackageAvailabilityBackgroundService>();
 
 var app = builder.Build(); 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+
 
 app.UseHttpsRedirection();
 app.UseCors("AllowFrontend");
