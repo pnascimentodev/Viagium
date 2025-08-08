88535e80152d7f19db61853a416cf2fad690a51b
Priscila Nascimento <pnmelo2808@gmail.com>
2025-08-07 21:12:39 -0300

    feat: update CORS policy and add Azure ARM template for app service deployment

--- Viagium/Program.cs
+++ Viagium/Program.cs
@@ -184,7 +184,8 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowFrontend",
         policy => policy
-            .WithOrigins("http://localhost:5173")
+            // .WithOrigins("http://localhost:5173")
+            .WithOrigins("https://viagium-fe.azurewebsites.net")
             .AllowAnyHeader()
             .AllowAnyMethod());
 });
